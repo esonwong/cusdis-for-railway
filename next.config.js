d942e65215f67f5fc1a5ffe9e49fb37108cc7c58
Eson Wong <95921719+esonwong@users.noreply.github.com>
2023-09-15 15:06:23 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 module.exports = {
+   async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      }
+  },
   rewrites() {
     return [
       {
